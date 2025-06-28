8026579238a37bca9d6a2f501c31d914c411051a
Aviek Paul <aviek17@gmail.com>
2025-06-28 22:36:31 +0530

    msw handlers file removed

--- src/api/msw/handlers/index.ts
+++ src/api/msw/handlers/index.ts
@@ -1,9 +1,3 @@
-import { authHandlers } from './authHandlers';
-import { blogHandlers } from './blogHandlers';
-import { developerHandlers } from './developerHandlers';
 
 export const handlers = [
-    ...authHandlers,
-    ...developerHandlers,
-    ...blogHandlers,
 ];
